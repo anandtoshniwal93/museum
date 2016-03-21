5664a02793a4d57be86164c5d75551514b68cd10
Anand <anand.toshniwal@qed42.com>
2016-03-21 19:17:45 +0530

    subtheme added

---
+++ sites/all/themes/museum/templates/page.tpl.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for a single Drupal page.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728148
+ */
+?>
+
+<div id="page">
+
+  <header class="header" id="header" role="banner">
+    <div class="header-section-1">
+   <?php print render($page['header']); ?>
+
+   </div>
+  </header>
+
+  <div class="content-preface" id="content-preface" >
+       <?php print render($page['content_preface']); ?>
+        </div>
+  <div id="main">
+
+    <div id="content" class="column" role="main">
+      <?php print render($page['content']); ?>
+    </div>
+
+<div class="content-postface" id="content-postface" >
+       <?php print render($page['content_postface']); ?>
+</div>
+
+<div class="footer-top" id="footer-top" >
+       <?php print render($page['footer_top']); ?>
+</div>
+
+</div>
+  <?php print render($page['footer']); ?>
+</div>
+
